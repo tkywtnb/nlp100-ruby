5c631730f9f90b14235be05dad80fb2ce50c109b
Takuya Watanabe <t-watanabe@esm.co.jp>
2017-03-13 11:58:08 +0900

    Solve 18

---
+++ chapter_2/18.rb
@@ -0,0 +1,8 @@
+require 'pathname'
+
+filepath = Pathname(__dir__).join('hightemp.txt')
+lines = File.readlines(filepath)
+
+lines.sort_by {|line| Float(line.split(/\t/)[2]) }.each do |line|
+  print line
+end

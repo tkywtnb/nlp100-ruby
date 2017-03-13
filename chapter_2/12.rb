cb4bd680839065bfba226ac75aa3e1e7a8c85e15
Takuya Watanabe <t-watanabe@esm.co.jp>
2017-03-13 18:20:54 +0900

    Fix bug

--- chapter_2/12.rb
+++ chapter_2/12.rb
@@ -1,12 +1,12 @@
 require 'csv'
+require 'fileutils'
 require 'pathname'
 
 filepath = Pathname(__dir__).join('hightemp.txt')
 col1path = Pathname(__dir__).join('col1.txt')
 col2path = Pathname(__dir__).join('col2.txt')
 
-File.delete col1path
-File.delete col2path
+FileUtils.rm_f [col1path, col2path]
 
 CSV.foreach filepath, col_sep: "\t" do |row|
   File.write col1path, row[0] + "\n", nil, mode: 'a'

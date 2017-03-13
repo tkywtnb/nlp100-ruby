1396eb8d2228ff8d52888a74ddc8d19a19187cc1
Takuya Watanabe <t-watanabe@esm.co.jp>
2017-03-13 18:22:37 +0900

    Fix sort order

--- chapter_2/18.rb
+++ chapter_2/18.rb
@@ -3,6 +3,6 @@ require 'pathname'
 filepath = Pathname(__dir__).join('hightemp.txt')
 lines = File.readlines(filepath)
 
-lines.sort_by {|line| Float(line.split(/\t/)[2]) }.each do |line|
+lines.sort_by {|line| -Float(line.split(/\t/)[2]) }.each do |line|
   print line
 end

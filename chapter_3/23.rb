44eebd28b64942648381b367e1fd6c8ed57d2712
Takuya Watanabe <t-watanabe@esm.co.jp>
2017-03-27 17:23:34 +0900

    Fix bug

--- chapter_3/23.rb
+++ chapter_3/23.rb
@@ -1,3 +1,3 @@
 puts $stdin
   .select {|line| line.chomp =~ /\A(={2,6})[^=]+\1\z/ }
-  .map {|line| line.gsub(/\A(={2,6})([^=]+)\1/) { "#{$1.size}: #{$2.strip}" }  }
+  .map {|line| line.gsub(/\A(={2,6})([^=]+)\1/) { "#{$1.size - 1}: #{$2.strip}" }  }
